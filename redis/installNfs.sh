4c3978a8973a3d60d24aadd4c4f850037733b560
192.168.0.68 <kylin27@126.com>
2019-12-30 23:18:02 +0800

    解决redis statefulset下的pod 因没有正确挂在nfs文件系统而不能启动的问题

--- redis/installNfs.sh
+++ redis/installNfs.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
 set -v
-
+#
+# /usr/local/k8s/redis/pv1 192.168.0.211(rw,sync,no_root_squash) 使只能192.168.0.211的主机
+# 才能挂在这个网络文件系统， 其他主机不能， 要改为*才能让其他主机挂在
 sudo cat << EOF > /tmp/exports 
-/usr/local/k8s/redis/pv1 192.168.0.211(rw,sync,no_root_squash)
-/usr/local/k8s/redis/pv2 192.168.0.211(rw,sync,no_root_squash)
-/usr/local/k8s/redis/pv3 192.168.0.211(rw,sync,no_root_squash)
-/usr/local/k8s/redis/pv4 192.168.0.211(rw,sync,no_root_squash)
-/usr/local/k8s/redis/pv5 192.168.0.211(rw,sync,no_root_squash)
-/usr/local/k8s/redis/pv6 192.168.0.211(rw,sync,no_root_squash)
+/usr/local/k8s/redis/pv1 *(rw,sync,no_root_squash)
+/usr/local/k8s/redis/pv2 *(rw,sync,no_root_squash)
+/usr/local/k8s/redis/pv3 *(rw,sync,no_root_squash)
+/usr/local/k8s/redis/pv4 *(rw,sync,no_root_squash)
+/usr/local/k8s/redis/pv5 *(rw,sync,no_root_squash)
+/usr/local/k8s/redis/pv6 *(rw,sync,no_root_squash)
 EOF
 sudo cp  /tmp/exports /etc/exports
 
@@ -17,6 +19,9 @@ sudo apt-get install nfs-common   -y      # 安装 NFS客户端
 sudo apt-get install rpcbind -y
 
 sudo mkdir -p /usr/local/k8s/redis/pv{1..6}
+
+# 解决其他主机挂载后，無法寫文件的錯誤
+chmod 777 /usr/local/k8s/redis/pv{1..6}
 
 sudo systemctl restart rpcbind
 #sudo systemctl restart nfs
