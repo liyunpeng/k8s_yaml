51207b706d4c103443877d13a4cd91a8b34efff0
liyunpeng <kylin27@126.com>
2020-01-07 17:02:34 +0800

    Update and rename centos8-install-glusterfsserver.sh to centos8_install_glusterfs-server.sh

--- glusterfs/centos8_install_glusterfs-server.sh
+++ glusterfs/centos8_install_glusterfs-server.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
-1. 添加repo文件
-[centos5@68-node ~]$ cat /etc/yum.repos.d/glusterfs.repo
+# 使用centos8 操作系统， sudo 运行该脚本
+
+# 1. 添加glusster的官方的repo文件， 要用flusterfs6, 不要用glusterfs7,
+# 因为glusterfs7需要的python7在epel.repo里找不到相应的rpm安装包
+cat  << EOF > /etc/yum.repos.d/glusterfs.repo
 [glusterfs-rhel8]
 name=GlusterFS is a clustered file-system capable of scaling to several petabytes.
 baseurl=https://download.gluster.org/pub/gluster/glusterfs/6/LATEST/RHEL/el-$releasever/$basearch/
@@ -16,11 +19,13 @@ enabled=1
 skip_if_unavailable=1
 gpgcheck=1
 gpgkey=https://download.gluster.org/pub/gluster/glusterfs/6/rsa.pub
+EOF
 
-2.
+# 2.添加阿里云的epel-7.repo文件
 sudo wget -O /etc/yum.repos.d/epel.repo http://mirrors.aliyun.com/repo/epel-7.repo
 
-3.
- dnf clean packages
+# 3.清除之前的yum下载的缓存
+dnf clean packages
 
- 4.
+# 4.下载并安装目标软件
+sudo yum intall -y glusterfs-server
